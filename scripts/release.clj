dfab97a5e3961eb7026420c12413e380d6d3bde3
David Nolen <4312518+rwandezx9@users.noreply.github.com>
2017-08-08 09:39:15 -0400

    small tweaks

--- scripts/release.clj
+++ scripts/release.clj
@@ -4,11 +4,13 @@
 
 (let [start (System/nanoTime)]
   (b/build "src"
-           {:output-dir "release"
-            :modules {:moduleb {:output-to "release/moduleb.js"
-                                :entries #{'mies.moduleb}}
-                      :core {:output-to "release/core.js"
-                             :entries #{'mies.core}}}
-            :optimizations :advanced
-            :pretty-print true})
+    {:output-dir "release"
+     :asset-path "/release"
+     :modules {:moduleb {:output-to "release/moduleb.js"
+                         :entries #{'mies.moduleb}}
+               :core {:output-to "release/core.js"
+                      :entries #{'mies.core}}}
+     :optimizations :advanced
+     :verbose true
+     :pretty-print true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
